067d54a3a6abc811f836199d4427ba75836739f1
Eugene Sokolyuk <evgnsklk@gmail.com>
2021-11-10 13:07:51 +0300

    add http exception filter

---
+++ finapi/Finance.Web.Api/Filters/HttpExceptionFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Net;
+
+namespace Finance.Web.Api.Filters
+{
+    public class HttpExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            HttpStatusCode code;
+            string message;
+            if (context.Exception is ArgumentException)
+            {
+                code = HttpStatusCode.BadRequest;
+                message = context.Exception.Message;
+            }
+            else
+            {
+                code = HttpStatusCode.InternalServerError;
+                message = "Unexpected server error. Please try again later.";
+            }
+
+            context.Result = new ObjectResult(new { message }) { StatusCode = (int)code };
+            context.ExceptionHandled = true;
+        }
+    }
+}
