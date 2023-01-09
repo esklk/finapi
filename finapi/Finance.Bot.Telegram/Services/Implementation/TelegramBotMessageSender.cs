fbfb9e7b3d4b22b4a35ccf938af204eba9115778
Yauheni Sakaliuk <yauheni.sakaliuk@externals.adidas.com>
2023-01-09 21:50:35 +0100

    manage callback button loaders, remove keyboard if no options sent

--- finapi/Finance.Bot.Telegram/Services/Implementation/TelegramBotMessageSender.cs
+++ finapi/Finance.Bot.Telegram/Services/Implementation/TelegramBotMessageSender.cs
@@ -9,29 +9,38 @@ namespace Finance.Bot.Telegram.Services.Implementation
     {
         private readonly ITelegramBotClient _botClient;
         private readonly long _chatId;
+        private readonly string? _queryId;
 
         public TelegramBotMessageSender(ITelegramBotClient botClient, IUpdateProvider updateProvider)
         {
             _botClient = botClient ?? throw new ArgumentNullException(nameof(botClient));
             _chatId = updateProvider.Update.GetChat().Id;
+            _queryId = updateProvider.Update.CallbackQuery?.Id;
         }
 
         public async Task SendAsync(BotMessage message)
         {
+            if (!string.IsNullOrWhiteSpace(_queryId))
+            {
+                // a workaround to remove a loader from callback button
+                await _botClient.AnswerCallbackQueryAsync(_queryId);
+            }
+
             await _botClient.SendTextMessageAsync(
                 chatId: _chatId,
                 text: message.Text,
                 replyMarkup: BuildReplyMarkup(message));
         }
 
-        public static IReplyMarkup? BuildReplyMarkup(BotMessage response)
+        public static IReplyMarkup BuildReplyMarkup(BotMessage response)
         {
             if (response.Options.Any())
             {
-                return new ReplyKeyboardMarkup(new[]
+                return new ReplyKeyboardMarkup(response.Options.Select(x => new KeyboardButton(x)).Chunk(2))
                 {
-                    response.Options.Select(x => new KeyboardButton(x))
-                });
+                    ResizeKeyboard = true,
+                    OneTimeKeyboard = true
+                };
             }
 
             if (response.TaggedOptions.Any())
@@ -41,7 +50,7 @@ namespace Finance.Bot.Telegram.Services.Implementation
                     .Chunk(2));
             }
 
-            return null;
+            return new ReplyKeyboardRemove();
         }
     }
 }
